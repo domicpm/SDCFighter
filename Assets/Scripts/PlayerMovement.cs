c1522e76929088fd3aec93138df9784722346869
domicpm <dominik.capellmann@gmail.com>
2025-06-04 23:05:31 +0200

    Controller Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -52,38 +52,58 @@ public class PlayerMovement : MonoBehaviour
 
         if (!isDead)
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
+            if (PauseManager.Instance.IsPaused)
+                return;
 
-            // Mausposition & Rotation
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
-
-            Vector3 movement = transform.position + new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime;
-            transform.position = movement;
-
-            // Heiltrank benutzen (Taste H)
-            if (Input.GetKeyDown(KeyCode.H))
+            if (!isDead)
             {
-                if (potamount > 0 && newhp < maxhp)
-                {
-                    newhp += healamount;
-                    if (newhp > maxhp) newhp = maxhp;
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
 
-                    potamount--;
-                    hp.setPlayerHealth(newhp);
-                    Hp.text = newhp.ToString();
+                 //Controller rechter Stick auslesen (XY)
+                float rightStickX = Input.GetAxis("RightStickHorizontal");
+                float rightStickY = Input.GetAxis("RightStickVertical");
+
+                Vector2 rightStickDir = new Vector2(rightStickX, rightStickY);
+
+                if (rightStickDir.magnitude > 0.1f) // Wenn Stick bewegt wird
+                {
+                    //Angle mit Stick - Richtung berechnen
+                      angle = Mathf.Atan2(rightStickY, rightStickX) * Mathf.Rad2Deg - 90f;
                 }
                 else
                 {
-                    Debug.Log("Keine Pots");
+                    // Sonst Maus verwenden wie bisher
+                    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
                 }
-            }
 
-            // Dash (Leertaste oder Controller)
-            if (Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.Space))
-            {
-                StartCoroutine(Dash());
+                Vector3 movement = transform.position + new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime;
+                transform.position = movement;
+
+                // Heiltrank benutzen (Taste H)
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    if (potamount > 0 && newhp < maxhp)
+                    {
+                        newhp += healamount;
+                        if (newhp > maxhp) newhp = maxhp;
+
+                        potamount--;
+                        hp.setPlayerHealth(newhp);
+                        Hp.text = newhp.ToString();
+                    }
+                    else
+                    {
+                        Debug.Log("Keine Pots");
+                    }
+                }
+
+                // Dash (Leertaste oder Controller)
+                if (Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.Space))
+                {
+                    StartCoroutine(Dash());
+                }
             }
         }
 
