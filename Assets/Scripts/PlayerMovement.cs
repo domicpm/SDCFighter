6c02aa4e6e82e7c9df0e6410ddfbb0abb915f389
domicpm <dominik.capellmann@gmail.com>
2025-06-05 19:09:09 +0200

    Update Map

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 mousePos;
     public float angle;
 
-    public const float maxhp = 100;
+    public const float maxhp = 40;
     public PlayerHealthBar healthbar;
     public float damageFromEnemy = 5;
     public Text Hp;
